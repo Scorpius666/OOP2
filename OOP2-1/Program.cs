046ca862bc8741ef695ce0e500b9e07d0ca4c2ea
Scorpius666 <mzydyx@outlook.com>
2020-07-14 22:09:08 +0800

    www

---
+++ OOP2-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;//=c语言头文件
+
+namespace OOP2_1//命名空间 Java:
+{
+    class Program//入口点 Entry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           
+            Console.WriteLine(); //printf();
+            
+            Console.ReadLine();//scanf();
+        }
+
+       
+    }
+    //抽离概念 抽象
+    //定义类
+    //概念实际化：实体 v。具体化
+    //概念的一个 实际的例子 ：实例
+    
+    class People
+    {
+        //成员变量
+
+        //成员变量：功能、动作
+
+    
+    }
+    //继承
+    class Student : People//People中的Student类
+    {
+
+    }
+}
